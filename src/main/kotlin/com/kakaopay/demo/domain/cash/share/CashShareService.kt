fc24a959ddb2a80a19749b45dc611639c95e27a1
kyu <gkaansrb@coupang.com>
2020-06-26 22:53:29 +0900

    QueryDsl 추가

--- src/main/kotlin/com/kakaopay/demo/domain/cash/share/CashShareService.kt
+++ src/main/kotlin/com/kakaopay/demo/domain/cash/share/CashShareService.kt
@@ -2,44 +2,50 @@ package com.kakaopay.demo.domain.cash.share
 
 import com.kakaopay.demo.api.share.CashShareOrderDto
 import com.kakaopay.demo.domain.cash.share.store.CashShareOrder
+import com.kakaopay.demo.domain.cash.share.store.CashShareOrderQueryDslRepository
 import com.kakaopay.demo.domain.cash.share.store.CashShareOrderRepository
 import kotlin.random.Random
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class CashShareService(private val cashShareOrderRepository: CashShareOrderRepository) {
+class CashShareService(
+    private val cashShareOrderRepository: CashShareOrderRepository,
+    private val cashShareOrderQueryDslRepository: CashShareOrderQueryDslRepository
+) {
 
     companion object {
         const val TOKEN_SIZE = 3
         val CHAR_SET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toByteArray()
     }
 
-    private fun generateToken() = (0..TOKEN_SIZE)
-        .map { CHAR_SET[Random.nextInt(0, CHAR_SET.size)] }
+    private fun generateToken() = (1..TOKEN_SIZE)
+        .map { CHAR_SET[Random.nextInt(0, CHAR_SET.size)].toChar() }
         .joinToString("")
 
-    private fun getToken(): String {
+    private fun getToken(roomId: String): String {
         var token = ""
         do {
             token = generateToken()
-        } while (cashShareOrderRepository.existsLookUpTargetToken(token).not())
+        } while (cashShareOrderQueryDslRepository.existsByValidToken(roomId = roomId, token = token))
         return token
     }
 
     @Transactional(readOnly = false)
-    fun create(userId: String, roomId: String, sharePerson: Long, shareAmount: Long): String = CashShareOrder.of(
-        token = getToken(),
+    fun create(userId: Long, roomId: String, sharePerson: Long, shareAmount: Long): String = CashShareOrder.of(
+        token = getToken(roomId),
         roomId = roomId,
         sharedPerson = sharePerson,
         cash = shareAmount,
         owner = userId
-    ).let { cashShareOrderRepository.save(it) }.token
+    ).apply { shared() }
+        .let { cashShareOrderRepository.save(it) }.token
 
     @Transactional(readOnly = false)
-    fun receipt(userId: String, roomId: String, token: String) =
-        cashShareOrderRepository.findOne(roomId, token).receipt(userId)
+    fun receipt(userId: Long, roomId: String, token: String) =
+        cashShareOrderQueryDslRepository.findByReceiptTarget(roomId, token)?.receipt(userId)
+            ?: throw Exception("뿌리기 정보를 찾을 수 없습니다.")
 
-    fun find(owner: String, roomId: String, token: String) =
-        cashShareOrderRepository.findOne(owner, roomId, token).let { CashShareOrderDto.of(it) }
+    fun find(owner: Long, roomId: String, token: String) =
+        cashShareOrderQueryDslRepository.findOne(owner, roomId, token)?.let { CashShareOrderDto.of(it) }
 }
--- src/main/kotlin/com/kakaopay/demo/domain/cash/share/store/CashShareOrderRepository.kt
+++ src/main/kotlin/com/kakaopay/demo/domain/cash/share/store/CashShareOrderRepository.kt
@@ -1,34 +1,5 @@
 package com.kakaopay.demo.domain.cash.share.store
 
-import java.time.LocalDateTime
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.querydsl.QuerydslPredicateExecutor
-import org.springframework.stereotype.Repository
 
-@Repository
-interface CashShareOrderRepository : JpaRepository<CashShareOrder, Long>, QuerydslPredicateExecutor<CashShareOrder> {
-
-    companion object {
-        val qCashShareOrder = QCashShareOrder.cashShareOrder
-    }
-
-    fun existsLookUpTargetToken(token: String): Boolean =
-        exists(
-            qCashShareOrder.token.eq(token).and(qCashShareOrder.lookUpDeadLine.goe(LocalDateTime.now()))
-        )
-
-    fun findOne(roomId: String, token: String): CashShareOrder =
-        findOne(
-            qCashShareOrder.token.eq(token)
-                .and(qCashShareOrder.lookUpDeadLine.goe(LocalDateTime.now()))
-                .and(qCashShareOrder.roomId.eq(roomId))
-        ).orElseThrow { Exception("뿌리기 정보를 찾을 수 없습니") }
-
-    fun findOne(owner: String, roomId: String, token: String): CashShareOrder =
-        findOne(
-            qCashShareOrder.token.eq(token)
-                .and(qCashShareOrder.lookUpDeadLine.goe(LocalDateTime.now()))
-                .and(qCashShareOrder.owner.eq(owner))
-                .and(qCashShareOrder.roomId.eq(roomId))
-        ).orElseThrow { Exception("뿌리기 정보를 찾을 수 없습니다") }
-}
+interface CashShareOrderRepository : JpaRepository<CashShareOrder, Long>
