ef11bb7f659b9a59ccd8374ecd1c41d994fccf6e
kyu <gkaansrb@coupang.com>
2020-06-30 22:02:49 +0900

    최대발급 초과시 에러 던지는 테스트

--- src/test/kotlin/com/kakaopay/demo/domain/cash/share/CashShareTokenGenerateComponentTest.kt
+++ src/test/kotlin/com/kakaopay/demo/domain/cash/share/CashShareTokenGenerateComponentTest.kt
@@ -4,6 +4,7 @@ import com.kakaopay.demo.domain.cash.share.store.CashShareOrderQueryDslRepositor
 import com.nhaarman.mockito_kotlin.any
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
@@ -27,6 +28,23 @@ internal class CashShareTokenGenerateComponentTest {
             }
 
             assert(duplicateTest.containsMatchIn(component.create("roomId")).not())
+        }
+    }
+
+    @Test
+    fun `최대 토큰 발급갯수 초과하면 에러를 뱉는다`() {
+        runBlocking {
+            Mockito.`when`(queryRepository.exists(any(), any())).thenAnswer {
+                true
+            }
+        }
+
+        assertThrows<IllegalStateException> {
+            runBlocking {
+                component.create("roomId")
+            }
+        }.apply {
+            assert(message == "최대 뿌리기 횟수[${CashShareTokenGenerateComponent.MAX_LOOP}]를 초과하였습니다. 일정 시간 이후 재 시도하시기 바랍니다.")
         }
     }
 }
